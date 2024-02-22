b162a099166f8775c43fbba3964bbb311b6f0114
Eric Richer <eric.richer@vistoconsulting.com>
2024-02-22 15:51:44 -0500

    Fixing baseurl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://lm-commons.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/lmcrbacmvc/',
+  baseUrl: '/lmc-rbac-mvc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
