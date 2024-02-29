2824801c60b4fca53a014dec7a8ade1269044491
Eric Richer <eric.richer@vistoconsulting.com>
2024-02-29 15:26:55 -0500

    CHanged ORM SQLite to in-memory storage

--- tests/testing.config.php
+++ tests/testing.config.php
@@ -21,7 +21,6 @@ return [
     'doctrine' => [
         'driver' => [
             'application_driver' => [
-//                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
                 'class' => 'Doctrine\ORM\Mapping\Driver\AttributeDriver',
                 'cache' => 'array',
                 'paths' => [__DIR__ . '/Asset']
@@ -43,9 +42,9 @@ return [
                     'password'      => null,
                     'dbname'        => 'test',
                     'driver'        => 'pdo_sqlite',
-                    'path'          => __DIR__ . "/../build/db.sqlite",
-//                    'path'          => null,
-//                    'memory'        => true,
+//                    'path'          => __DIR__ . "/../build/db.sqlite",
+                    'path'          => null,
+                    'memory'        => true,
                 ],
             ],
         ],
